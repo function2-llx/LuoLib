f5828605d256fa9c1dceae4da8fd0d1313485293
leezythu <1491547565@qq.com>
2022-05-03 07:00:48 +0000

    implement vit

--- umei/umei.py
+++ umei/umei.py
@@ -26,17 +26,18 @@ class UMeI(LightningModule):
         self.args = args
         self.encoder = encoder
         self.decoder = decoder
-        if args.model_name=='resnet':
+        self.model_name = args.model_name
+        if self.model_name=='resnet':
             self.cls_head = nn.Linear(encoder.cls_feature_size + args.clinical_feature_size, args.num_cls_classes)
-        elif args.model_name=='vit':
+        elif self.model_name=='vit':
             self.cls_head = nn.Sequential(nn.Linear(args.hidden_size + args.clinical_feature_size, args.num_cls_classes), nn.Tanh())
         else:
             raise NotImplementedError
         self.cls_loss_fn = nn.CrossEntropyLoss()
 
-        if args.model_name=='resnet':
+        if self.model_name=='resnet':
             nn.init.constant_(torch.as_tensor(self.cls_head.bias), 0)
-        elif args.model_name=='vit':
+        elif self.model_name=='vit':
             nn.init.constant_(torch.as_tensor(list(self.cls_head._modules.items())[0][1].bias), 0)
         else:
             raise NotImplementedError
@@ -53,9 +54,12 @@ class UMeI(LightningModule):
         encoder_out: UEncoderOutput = self.encoder(batch[self.args.img_key])
         ret = {'loss': torch.tensor(0., device=self.device)}
         if self.args.cls_key in batch:
-            #for vit
-            cls_out = self.cls_head(torch.cat((encoder_out[0][:,0], batch[self.args.clinical_key]), dim=1))
-            # cls_out = self.cls_head(torch.cat((encoder_out.cls_feature, batch[self.args.clinical_key]), dim=1))
+            if self.model_name=='resnet':
+                cls_out = self.cls_head(torch.cat((encoder_out.cls_feature, batch[self.args.clinical_key]), dim=1))
+            elif self.model_name=='vit':
+                cls_out = self.cls_head(torch.cat((encoder_out[0][:,0], batch[self.args.clinical_key]), dim=1))
+            else:
+                raise NotImplementedError
             cls_loss = self.cls_loss_fn(cls_out, batch[self.args.cls_key])
             # self.log('cls_loss', cls_loss, prog_bar=True)
             ret['loss'] += cls_loss * self.args.cls_loss_factor
