be95f0a38b1234ba92bca9d3380578ae6d281441
function2 <luolx21@mails.tsinghua.edu.cn>
2022-07-12 13:22:17 +0800

    add ssl conf; update swin_mae

--- umei/swin_mae/args.py
+++ umei/swin_mae/args.py
@@ -1,6 +1,14 @@
 from dataclasses import field
 
+import numpy as np
+
 from umei.args import UMeIArgs
 
 class SwinMAEArgs(UMeIArgs):
     num_input_channels: int = field(default=1)
+    mask_ratio: float = field(default=0.75)
+    mask_size: int = field(default=16)
+
+    @property
+    def mask_num(self) -> int:
+        return int(np.log(1 - self.mask_ratio) / np.log(1 - self.mask_size ** 3 / np.product(self.sample_shape)))
--- umei/swin_mae/model.py
+++ umei/swin_mae/model.py
@@ -35,7 +35,25 @@ class SwinMAE(pl.LightningModule):
     def initialize_weights(self):
         # initialize patch_embed like nn.Linear (instead of nn.Convnd)
         w: torch.Tensor = self.encoder.patch_embed.weight.data
-        torch.nn.init.xavier_uniform_(w)
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        torch.nn.init.normal_(self.mask_token, std=.02)
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m: nn.Module):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0)
+
+    def random_masking(self, x: torch.Tensor):
+        mask_num = self.args.mask_num
+        noise = torch.rand(x.shape[:-1], device=self.device)
 
 
     def training_step(self, x: torch.Tensor, *args, **kwargs):
