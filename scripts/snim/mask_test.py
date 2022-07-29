7271aadd214ff818b9cf4774fcf94aaa21af505a
function2 <luolx21@mails.tsinghua.edu.cn>
2022-07-30 00:28:10 +0800

    update mask test

--- scripts/snim/mask_test.py
+++ scripts/snim/mask_test.py
@@ -13,16 +13,16 @@ def main():
     model = SnimModel(args).cuda().eval()
     torch.backends.cudnn.benchmark = True
     batch_size = args.train_batch_size
-    print('expected mask ratio:', args.mask_ratio)
+    print(f'expected mask ratio: {args.mask_ratio * 100}%')
     results = []
     with torch.no_grad():
         for _ in trange(1000, ncols=80):
             mask = model.gen_patch_mask(batch_size, args.sample_shape)
             mask = mask.view(batch_size, -1)
-            results.append(mask.sum(dim=-1) / mask.shape[1])
+            results.append(mask.sum(dim=-1) * 100 / mask.shape[1])
 
     results = torch.cat(results)
-    print(f'{results.mean().item()}±{results.std().item()}')
+    print(f'{results.shape[0]} samples mean±std: {results.mean().item():.3f}%±{results.std().item():.3f}%')
 
 if __name__ == '__main__':
     main()
