c59be01b9ffbdad2f936a0929a54abc92d274fa6
function2 <function2-llx@outlook.com>
2023-07-26 21:28:58 +0800

    update load ckpt

--- luolib/models/utils.py
+++ luolib/models/utils.py
@@ -5,15 +5,16 @@ import torch
 from torch import nn
 
 from luolib.types import NoWeightDecayParameter, ParamGroup
+from monai.config import PathLike
 
-def load_ckpt(model: nn.Module, ckpt_or_path: dict | Path | None, state_dict_key: str | None = None, key_prefix: str = ''):
+def load_ckpt(model: nn.Module, ckpt_or_path: dict | PathLike | None, state_dict_key: str | None = None, key_prefix: str = ''):
     if ckpt_or_path is None:
         return
-    if isinstance(ckpt_or_path, Path):
-        ckpt: dict = torch.load(ckpt_or_path, map_location='cpu')
-    else:
+    if isinstance(ckpt_or_path, dict):
         ckpt = ckpt_or_path
-    if state_dict_key is None and isinstance(ckpt, dict):
+    else:
+        ckpt: dict = torch.load(ckpt_or_path, map_location='cpu')
+    if state_dict_key is None:
         if 'state_dict' in ckpt:
             state_dict_key = 'state_dict'
         elif 'model' in ckpt:
@@ -26,7 +27,7 @@ def load_ckpt(model: nn.Module, ckpt_or_path: dict | Path | None, state_dict_key
         if k.startswith(key_prefix)
     }
     missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
-    if isinstance(ckpt_or_path, Path):
+    if not isinstance(ckpt_or_path, dict):
         print(f'Loaded {state_dict_key} from checkpoint {ckpt_or_path}')
     print('missing keys:', missing_keys)
     print('unexpected keys:', unexpected_keys)
