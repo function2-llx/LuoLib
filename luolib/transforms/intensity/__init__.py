eb175fbddd90952356c09d1db665e1123dbc2bf9
function2 <function2-llx@outlook.com>
2023-08-08 22:24:40 +0800

    create foreground mask

--- luolib/transforms/intensity/__init__.py
+++ luolib/transforms/intensity/__init__.py
@@ -1,3 +1,4 @@
 from .adjust_contrast import RandAdjustContrastD
 from .clamp import ClampIntensityD
 from .gamma_correction import RandGammaCorrectionD
+from .foreground_mask import CreateForegroundMaskD
