b1f5db6ae0b1e3bec1ba2641a7f46f1506c5d64b
function2 <luolx21@mails.tsinghua.edu.cn>
2023-11-21 18:56:15 +0800

    do not import torch.multiprocessing in top level

--- luolib/utils/device_map.py
+++ luolib/utils/device_map.py
@@ -1,5 +1,3 @@
-from multiprocessing import Manager
-from multiprocessing.managers import SyncManager
 import os
 
 import torch.cuda
@@ -9,17 +7,30 @@ __all__ = [
 ]
 
 num_devices = torch.cuda.device_count()
-manager: SyncManager = Manager()
-pid_to_device_id = manager.dict()
-device_ref_count = manager.list([0 for _ in range(num_devices)])
-lock = manager.Lock()
+
+class DeviceMap:
+    def __init__(self):
+        from multiprocessing import Manager
+        from multiprocessing.managers import SyncManager
+        manager: SyncManager = Manager()
+        self.pid_to_device_id = manager.dict()
+        self.device_ref_count = manager.list([0 for _ in range(num_devices)])
+        self.lock = manager.Lock()
+
+    def get(self):
+        pid = os.getpid()
+        if (device_id := self.pid_to_device_id.get(pid)) is None:
+            with self.lock:
+                device_id = min(range(num_devices), key=lambda i: self.device_ref_count[i])
+                self.pid_to_device_id[pid] = device_id
+                self.device_ref_count[device_id] += 1
+            torch.cuda.set_device(device_id)
+        return torch.device(device_id)
+
+_device_map: DeviceMap | None = None
 
 def device_map():
-    pid = os.getpid()
-    if (device_id := pid_to_device_id.get(pid)) is None:
-        with lock:
-            device_id = min(range(num_devices), key=lambda i: device_ref_count[i])
-            pid_to_device_id[pid] = device_id
-            device_ref_count[device_id] += 1
-        torch.cuda.set_device(device_id)
-    return torch.device(device_id)
+    global _device_map
+    if _device_map is None:
+        _device_map = DeviceMap()
+    return _device_map.get()
