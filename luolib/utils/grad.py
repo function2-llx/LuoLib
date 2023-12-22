e96197b3468ef7ea65e569d2185fd46d647b6a12
function2 <function2-llx@outlook.com>
2023-12-22 13:01:40 +0800

    grad norm no grad

--- luolib/utils/grad.py
+++ luolib/utils/grad.py
@@ -1,10 +1,11 @@
 import torch
 from torch import nn
 
+@torch.no_grad()
 def grad_norm(m: nn.Module):
     norm = 0.
     for p in m.parameters():
         if p.grad is not None:
-            grad = p.grad.detach().flatten()
+            grad = p.grad.flatten()
             norm += torch.dot(grad, grad)
     return norm ** 0.5
