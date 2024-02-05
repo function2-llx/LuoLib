20a61590169d66bb1d474ee8db5551824f9aa009
function2 <function2-llx@outlook.com>
2024-02-05 15:38:31 +0800

    update quantile: support dim and keepdim

--- luolib/transforms/upnu.py
+++ luolib/transforms/upnu.py
@@ -6,8 +6,12 @@ __all__ = [
     'quantile'
 ]
 
-def quantile(x: torch.Tensor, q: float) -> torch.Tensor:
+def quantile(x: torch.Tensor, q: float, dim: int | None = None, keepdim: bool = False) -> torch.Tensor:
     # workaround for https://github.com/pytorch/pytorch/issues/64947
+    if dim is None:
+        x = x.view(-1)
+        k = round(x.numel() * q)
+    else:
+        k = round(x.shape[dim] * q)
     assert 0 <= q <= 1
-    k = round(x.numel() * q)
-    return x.view(-1).kthvalue(k).values
+    return x.kthvalue(k, dim, keepdim).values
