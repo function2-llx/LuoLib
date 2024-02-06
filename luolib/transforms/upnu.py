d499cbb0a09aad791c44881978c9ab1191f284e1
function2 <function2-llx@outlook.com>
2024-02-06 14:06:07 +0800

    enhance quantile

--- luolib/transforms/upnu.py
+++ luolib/transforms/upnu.py
@@ -8,10 +8,13 @@ __all__ = [
 
 def quantile(x: torch.Tensor, q: float, dim: int | None = None, keepdim: bool = False) -> torch.Tensor:
     # workaround for https://github.com/pytorch/pytorch/issues/64947
+    assert 0 <= q <= 1
     if dim is None:
         x = x.view(-1)
         k = round(x.numel() * q)
+        dim = 0
     else:
         k = round(x.shape[dim] * q)
-    assert 0 <= q <= 1
+    if k == 0:
+        k = 1
     return x.kthvalue(k, dim, keepdim).values
