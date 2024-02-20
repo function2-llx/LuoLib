e99d66d32b763ea7d42df82b37c0d0e84570f376
function2 <function2-llx@outlook.com>
2024-02-20 10:51:12 +0800

    add ensure_rgb

--- luolib/utils/misc.py
+++ luolib/utils/misc.py
@@ -1,5 +1,6 @@
 from typing import TypeVar
 
+from einops import einops
 import torch
 
 __all__ = [
@@ -19,3 +20,13 @@ RGB_TO_GRAY_WEIGHT = (0.299, 0.587, 0.114)
 
 def ema_update(ema: torch.Tensor, x: torch.Tensor, decay: float):
     return ema.mul_(decay).add_(x, alpha=1 - decay)
+
+def ensure_rgb(x: torch.Tensor, batched: bool = False) -> tuple[torch.Tensor, bool]:
+    if x.shape[batched] == 3:
+        not_rgb = False
+    else:
+        assert x.shape[batched] == 1
+        maybe_batch = 'n' if batched else ''
+        x = einops.repeat(x, f'{maybe_batch} 1 ... -> c ...', c=3)
+        not_rgb = True
+    return x, not_rgb
