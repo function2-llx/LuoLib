254442e7de61cb6fd297ac02dee91e12bdd5b817
function2 <function2-llx@outlook.com>
2024-03-05 23:17:35 +0800

    enhance nnUNetLoader: class_locations handling; remove_label

--- luolib/transforms/io.py
+++ luolib/transforms/io.py
@@ -1,6 +1,7 @@
 from collections.abc import Hashable
 from pathlib import Path
 
+import cytoolz
 import numpy as np
 import pandas as pd
 
@@ -31,12 +32,16 @@ class nnUNetLoader(mt.Transform):
         data_dir: Path | None = None,
         img_key: Hashable | None = 'img',
         seg_key: Hashable | None = 'seg',
+        unravel_class_locations: bool = False,
+        remove_label: bool = True,
         allow_missing: bool = False,
     ):
         self.data_dir = data_dir
         self.img_key = img_key
         self.seg_key = seg_key
         keys = [*filter(lambda key: key is not None, [img_key, seg_key])]
+        self.unravel_class_locations = unravel_class_locations
+        self.remove_label = remove_label
         self.img_loader = mt.LoadImageD(
             keys,
             reader=NumpyReader,
@@ -53,7 +58,20 @@ class nnUNetLoader(mt.Transform):
         if self.seg_key is not None and (seg_path := data_dir / f'{key}_seg.npy').exists():
             path_data[self.seg_key] = seg_path
         img_data = self.img_loader(path_data)
-        meta = pd.read_pickle(data_dir / f'{key}.pkl')
+        if self.remove_label and (seg := img_data.get('seg')) is not None:
+            # https://github.com/MIC-DKFZ/nnUNet/blob/v2.3.1/nnunetv2/preprocessing/cropping/cropping.py#L43
+            seg[seg < 0] = 0
+
+        meta: dict = pd.read_pickle(data_dir / f'{key}.pkl')
+        if self.unravel_class_locations:
+            class_locations: dict = meta['class_locations']
+            shape = cytoolz.first(img_data.values()).shape[1:]
+            meta['class_locations'] = [
+                np.ravel_multi_index(locations[:, 1:].T, shape)
+                if len(locations := class_locations[k]) > 0 else np.array([])
+                for k in sorted(class_locations.keys())
+            ]
+
         return {**img_data, **meta, 'path_base': str(data_dir / key)}
 
 class nnUNetLoaderD(mt.Transform):
@@ -63,6 +81,7 @@ class nnUNetLoaderD(mt.Transform):
         data_dir: Path | None = None,
         img_key: Hashable | None = 'img',
         seg_key: Hashable | None = 'seg',
+        unravel_class_locations: bool = False,
         allow_missing: bool = False,
     ):
         """
@@ -70,7 +89,13 @@ class nnUNetLoaderD(mt.Transform):
             key: the key to obtain nnU-Net case key
         """
         self.key = key
-        self.loader = nnUNetLoader(data_dir, img_key, seg_key, allow_missing)
+        self.loader = nnUNetLoader(
+            data_dir=data_dir,
+            img_key=img_key,
+            seg_key=seg_key,
+            unravel_class_locations=unravel_class_locations,
+            allow_missing=allow_missing,
+        )
 
     def __call__(self, data: dict, data_dir: Path | None = None):
         data = dict(data)
