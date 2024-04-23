b679ac3492190b7a359d0986ff3ce5abcb7d319c
function2 <function2-llx@outlook.com>
2024-04-23 17:23:17 +0800

    iterate name in grad norm for debugging

--- luolib/utils/grad.py
+++ luolib/utils/grad.py
@@ -4,7 +4,7 @@ from torch import nn
 @torch.no_grad()
 def grad_norm(m: nn.Module):
     norm = 0.
-    for p in m.parameters():
+    for name, p in m.named_parameters():
         if p.grad is not None:
             grad = p.grad.flatten()
             norm += torch.dot(grad, grad)
