abf1156c415ccdd28924f809a22c19987802209f
function2 <function2-llx@outlook.com>
2024-04-25 12:29:20 +0800

    enable atomic save

--- luolib/utils/zstd.py
+++ luolib/utils/zstd.py
@@ -9,10 +9,15 @@ __all__ = [
     'load_pt_zst',
 ]
 
-def save_pt_zst(x: ..., path: Path):
-    with BytesIO() as buffer, open(path, 'wb') as f:
-        torch.save(x, buffer)
-        f.write(zstd.compress(buffer.getvalue()))
+def save_pt_zst(x: ..., path: Path, atomic: bool = False):
+    if atomic:
+        tmp_path = path.with_name(f'.{path.name}')
+        save_pt_zst(x, tmp_path, atomic=False)
+        tmp_path.rename(path)
+    else:
+        with BytesIO() as buffer, open(path, 'wb') as f:
+            torch.save(x, buffer)
+            f.write(zstd.compress(buffer.getvalue()))
 
 def load_pt_zst(path: Path):
     with open(path, 'rb') as f:
