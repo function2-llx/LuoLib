517c1d3987738fa263d78c29fa99e6934891c0d5
function2 <function2-llx@outlook.com>
2024-05-05 03:09:33 +0800

    fix box ops: flip

--- luolib/transforms/box_ops.py
+++ luolib/transforms/box_ops.py
@@ -5,7 +5,19 @@ import torch
 
 from monai.apps.detection.transforms.box_ops import apply_affine_to_boxes
 from monai.config import NdarrayOrTensor, NdarrayTensor
+from monai.data.box_utils import TO_REMOVE
 from monai.utils import convert_to_dst_type, convert_to_tensor
+
+assert TO_REMOVE == 0
+
+def apply_affine_to_boxes_int(boxes: NdarrayTensor, affine: NdarrayOrTensor) -> NdarrayTensor:
+    boxes_t: torch.Tensor = convert_to_tensor(boxes)
+    boxes_t[:, 3:] -= 1
+    boxes_f = apply_affine_to_boxes(boxes, affine)
+    boxes_t = boxes_f.floor().long()
+    boxes_t[:, 3:] += 1
+    boxes, *_ = convert_to_dst_type(boxes_t, boxes)
+    return boxes
 
 def convert_boxes_to_int(boxes: NdarrayTensor) -> NdarrayTensor:
     boxes_t = convert_to_tensor(boxes)
@@ -14,11 +26,6 @@ def convert_boxes_to_int(boxes: NdarrayTensor) -> NdarrayTensor:
     boxes_int[:, 3:] = boxes_t[:, 3:].ceil()
     boxes_int, *_ = convert_to_dst_type(boxes_int, boxes, dtype=torch.int64)
     return boxes_int
-
-def apply_affine_to_boxes_int(boxes: NdarrayTensor, affine: NdarrayOrTensor) -> NdarrayTensor:
-    boxes_f = apply_affine_to_boxes(boxes, affine)
-    boxes = convert_boxes_to_int(boxes_f)
-    return boxes
 
 def norm_boxes(boxes: NdarrayTensor, norm_size: Sequence[int]):
     boxes_t = convert_to_tensor(boxes)
